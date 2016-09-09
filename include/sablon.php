01de6c45075ee7f0a37f3428b38af744e71e6764
Metin Ağaoğlu <metnagaoglu@gmail.com>
2016-09-09 20:31:11 +0300

    Update sablon.php

--- include/sablon.php
+++ include/sablon.php
@@ -34,6 +34,7 @@ $siteUrl = "http://localhost/maps";
       <ul class="nav navbar-nav">
         <li <?php if($page == 1): echo 'class=active'; endif;?>><a href="<?=$siteUrl?>">Anasayfa <span class="sr-only">(current)</span></a></li>
         <li <?php if($page == 3): echo 'class=active'; endif;?>><a href="<?=$siteUrl?>/kaydet.php">Kaydet</a></li>
+        <li <?php if($page == 4): echo 'class=active'; endif;?>/yakinimdakiler.php">Yakınımdakiler</a></li>
       </ul>
     </div><!-- /.navbar-collapse -->
   </div><!-- /.container-fluid -->
