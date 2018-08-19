554643b412467d4c42052b28a0f1c411c409d7ff
Metin Ağaoğlu <metin.agaoglu@coslat.com>
2018-08-19 14:25:37 +0300

    Pathler Düzeltildi.

--- include/sablon.php
+++ include/sablon.php
@@ -1,6 +1,5 @@
 <?php  
 include 'db.php';
-$siteUrl = "http://localhost/maps";
 
 ?>
 <?php  function headers($title,$page){  
@@ -10,9 +9,9 @@ $siteUrl = "http://localhost/maps";
 <html>
 <head>
 	<title><?=$title?></title>
-	<link rel="stylesheet" type="text/css" href="<?=$siteUrl?>/css/bootstrap-min.css">
-	<link rel="stylesheet" type="text/css" href="<?=$siteUrl ?>/css/bootstrap-theme-min.css">
-	<script src='<?=$siteUrl?>/js/jquery.js'></script>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap-min.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap-theme-min.css">
+	<script src='js/jquery.js'></script>
 	<script src="//api-maps.yandex.ru/2.1/?lang=tr_TR" type="text/javascript"></script>
 </head>
 <body>
@@ -32,9 +31,9 @@ $siteUrl = "http://localhost/maps";
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
-        <li <?php if($page == 1): echo 'class=active'; endif;?>><a href="<?=$siteUrl?>">Anasayfa <span class="sr-only">(current)</span></a></li>
-        <li <?php if($page == 3): echo 'class=active'; endif;?>><a href="<?=$siteUrl?>/kaydet.php">Kaydet</a></li>
-        <li <?php if($page == 4): echo 'class=active'; endif;?>><a href="<?=$siteUrl?>/yakinimdakiler.php">Yakınımdakiler</a></li>
+        <li <?php if($page == 1): echo 'class=active'; endif;?>><a href="index.php">Anasayfa <span class="sr-only">(current)</span></a></li>
+        <li <?php if($page == 3): echo 'class=active'; endif;?>><a href="kaydet.php">Kaydet</a></li>
+        <li <?php if($page == 4): echo 'class=active'; endif;?>><a href="yakinimdakiler.php">Yakınımdakiler</a></li>
       </ul>
     </div><!-- /.navbar-collapse -->
   </div><!-- /.container-fluid -->
@@ -43,7 +42,7 @@ $siteUrl = "http://localhost/maps";
 function footers(){
 	global $siteUrl;
 ?>
-<script src='<?=$siteUrl?>/js/bootstrap-min.js'></script>
+<script src='js/bootstrap-min.js'></script>
 </body>
 </html>
 <?php  } ?>
