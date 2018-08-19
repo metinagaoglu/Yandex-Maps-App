32a57cbb9c7e39a5fb5e707a5c2f92d46c13b259
Metin Ağaoğlu <metin.agaoglu@coslat.com>
2018-08-19 14:29:42 +0300

    Change App Name

--- include/sablon.php
+++ include/sablon.php
@@ -25,7 +25,7 @@ include 'db.php';
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="#">Emlak </a>
+      <a class="navbar-brand" href="#">Yandex Map Example </a>
     </div>
 
     <!-- Collect the nav links, forms, and other content for toggling -->
